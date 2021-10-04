362783fa6d58a67e7c9de85e630262663a6af19c
Niels Faurskov <niels@faurskov.dev>
2021-10-04 13:44:58 +0200

    init

---
+++ config/sdu-mfa.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'client_id'          => env('MFA_CLIENT_ID'),
+    'client_secret'      => env('MFA_CLIENT_SECRET'),
+    'tenant_id'          => env('MFA_TENANT_ID'),
+    'required_ad_groups' => env('MFA_REQUIRED_AD_GROUPS')
+];
