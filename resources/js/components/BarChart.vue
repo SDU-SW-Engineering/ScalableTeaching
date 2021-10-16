b5eca5e50f1669f7eb0f674a8032ce0f7d6cf83a
Niels Faurskov <niels@faurskov.dev>
2021-10-16 13:37:15 +0200

    Graph dark mode support

--- resources/js/components/BarChart.vue
+++ resources/js/components/BarChart.vue
@@ -4,39 +4,58 @@ import {Bar} from 'vue-chartjs'
 export default {
     extends: Bar,
     props: ['data', 'labels'],
+    data() {
+        return {
+            darkMode: window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches
+        }
+    },
     mounted() {
-        this.renderChart({
-            datasets: this.data,
-            labels: this.labels
-        }, {
-            responsive: true,
-            maintainAspectRatio: false,
-            tooltips: {
-                mode: "index",
-            },
-            legend: {
-                display: false
-            },
-            scales: {
-                xAxes: [
-                    {
-                        stacked: true,
-                        ticks: {
-                            display: false
-                        },
-                        gridLines: {
-                            display: false
+        window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', e => {
+            this.darkMode = e.matches;
+            this.render();
+        });
+        this.render()
+    },
+    methods: {
+        render: function () {
+            this.renderChart({
+                datasets: this.data,
+                labels: this.labels
+            }, {
+                responsive: true,
+                maintainAspectRatio: false,
+                tooltips: {
+                    mode: "index",
+                },
+                legend: {
+                    display: false
+                },
+                scales: {
+                    xAxes: [
+                        {
+                            stacked: true,
+                            ticks: {
+                                display: false
+                            },
+                            gridLines: {
+                                display: false
+                            }
                         }
-                    }
-                ],
-                yAxes: [
-                    {
-                        stacked: true,
-                        gridLines: {}
-                    }
-                ]
-            }
-        })
+                    ],
+                    yAxes: [
+                        {
+                            ticks: {
+                                fontColor: this.darkMode ? "#ffffff" : '#6A6A6A'
+                            },
+                            stacked: true,
+                            gridLines: {
+                                color: this.darkMode ? "#2f2f2f" : '#ECECEC'
+                            }
+                        }
+                    ]
+                }
+            })
+        }
     }
 }
 </script>
