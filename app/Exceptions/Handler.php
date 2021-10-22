779a1ba8cbed9e43d32bdb3303f1d104dd554abb
Niels Faurskov <niels@faurskov.dev>
2021-10-22 03:25:33 +0200

    Enabled sentry error tracking and performance monitoring

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -38,4 +38,13 @@ class Handler extends ExceptionHandler
             //
         });
     }
+
+    public function report(Throwable $e)
+    {
+        if (app()->bound('sentry') && $this->shouldReport($e)) {
+            app('sentry')->captureException($e);
+        }
+
+        parent::report($e);
+    }
 }
