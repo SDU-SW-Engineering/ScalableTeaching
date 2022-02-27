0698bad297d4ad19a817a288ad9fd82b42435caf
Niels Faurskov <niels@faurskov.dev>
2022-02-27 16:13:04 +0100

    Further testing

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -11,6 +11,7 @@ use GraphQL\SchemaObject\RepositoryBlobsArgumentsObject;
 use GraphQL\SchemaObject\RepositoryTreeArgumentsObject;
 use GraphQL\SchemaObject\RootProjectsArgumentsObject;
 use GraphQL\SchemaObject\RootQueryObject;
+use Illuminate\Contracts\Support\Arrayable;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -20,13 +21,14 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 /**
- * @property SubTaskCollection $sub_tasks
+ * @throws ModelNotFoundException<Task>
+ * }
+ *
  * @property CorrectionType $correction_type
  * @property string $name
  * @method Task findOrFail($id, $columns = []) {
- *     @throws ModelNotFoundException<Task>
- * }
- *
+ * @property SubTaskCollection $sub_tasks
+ * @property-read CourseTrack|null $track
  */
 class Task extends Model
 {
@@ -48,11 +50,11 @@ class Task extends Model
     public function reloadDescriptionFromRepo()
     {
         $gitlabManager = app(GitLabManager::class);
-        $project       = $gitlabManager->projects()->show($this->source_project_id);
-        $branch        = $project['default_branch'];
-        $readme        = base64_decode($gitlabManager->repositoryFiles()->getFile($this->source_project_id, 'README.md', $branch)['content']);
-        $parseDown     = new \Parsedown();
-        $htmlReadme    = $parseDown->parse($readme);
+        $project = $gitlabManager->projects()->show($this->source_project_id);
+        $branch = $project['default_branch'];
+        $readme = base64_decode($gitlabManager->repositoryFiles()->getFile($this->source_project_id, 'README.md', $branch)['content']);
+        $parseDown = new \Parsedown();
+        $htmlReadme = $parseDown->parse($readme);
 
         $this->update([
             'description'          => $htmlReadme,
@@ -61,7 +63,7 @@ class Task extends Model
     }
 
     // region relationships
-    public function course() : BelongsTo
+    public function course(): BelongsTo
     {
         return $this->belongsTo(Course::class);
     }
@@ -71,7 +73,7 @@ class Task extends Model
         return $this->hasManyThrough(Pipeline::class, Project::class)->withTrashedParents();
     }
 
-    public function grades() : HasMany
+    public function grades(): HasMany
     {
         return $this->hasMany(Grade::class);
     }
@@ -80,22 +82,23 @@ class Task extends Model
     {
         return $this->belongsTo(CourseTrack::class);
     }
+
     // endregion
 
-    public function dailyBuilds(bool $withTrash = false, $withToday = false) : \Illuminate\Support\Collection
+    public function dailyBuilds(bool $withTrash = false, $withToday = false): \Illuminate\Support\Collection
     {
         $query = $this->jobs();
-        if ($withTrash)
+        if($withTrash)
             $query->withTrashedParents();
-        return $query->daily($this->starts_at->startOfDay(), $this->earliestEndDate(! $withToday))->get();
+        return $query->daily($this->starts_at->startOfDay(), $this->earliestEndDate(!$withToday))->get();
     }
 
-    public function projects() : HasMany
+    public function projects(): HasMany
     {
         return $this->hasMany(Project::class);
     }
 
-    public function protectedFiles() : HasMany
+    public function protectedFiles(): HasMany
     {
         return $this->hasMany(TaskProtectedFile::class);
     }
@@ -124,22 +127,21 @@ class Task extends Model
         return now()->isAfter($this->ends_at) ? $this->ends_at : ($excludeToday ? now()->subDay() : now());
     }
 
-    public function currentProjectForUser(User $user) : ?Project
+    public function currentProjectForUser(User $user): ?Project
     {
-        $myGroups     = $this->course->groups()
+        $myGroups = $this->course->groups()
             ->whereRelation('users', 'user_id', $user->id)
             ->latest()
             ->pluck('name', 'id');
-        $groupProject = $this->projects()->whereHasMorph('ownable', Group::class, function (Builder $query) use ($myGroups)
-        {
+        $groupProject = $this->projects()->whereHasMorph('ownable', Group::class, function(Builder $query) use ($myGroups) {
             $query->whereIn('id', $myGroups->keys());
         })->first();
 
-        if ($groupProject != null)
+        if($groupProject != null)
             return $groupProject;
 
         return $this->projects()
-            ->whereHasMorph('ownable', User::class, fn (Builder $query) => $query->where('id', $user->id))
+            ->whereHasMorph('ownable', User::class, fn(Builder $query) => $query->where('id', $user->id))
             ->first();
     }
 
@@ -147,21 +149,19 @@ class Task extends Model
      * @param Collection $users
      * @return Collection
      */
-    public function progressStatus(Collection $users) : Collection
+    public function progressStatus(Collection $users): Collection
     {
-        return $users->filter(function (User $user)
-        {
+        return $users->filter(function(User $user) {
             return $this->currentProjectForUser($user) != null;
         });
     }
 
-    public function projectsForUsers(Collection $users) : Collection
+    public function projectsForUsers(Collection $users): Collection
     {
         $projects = Collection::empty();
-        $users->each(function (User $user) use ($projects)
-        {
+        $users->each(function(User $user) use ($projects) {
             $project = $this->currentProjectForUser($user);
-            if ($project == null)
+            if($project == null)
                 return;
             $projects[] = $project;
         });
@@ -182,34 +182,31 @@ class Task extends Model
             ->selectNodes()
             ->selectName()
             ->selectSha();
-        $client   = new Client('https://gitlab.sdu.dk/api/graphql', ["Authorization" => 'Bearer ' . config('scalable.gitlab_token')]);
+        $client = new Client('https://gitlab.sdu.dk/api/graphql', ["Authorization" => 'Bearer ' . config('scalable.gitlab_token')]);
         $projects = $client->runQuery($rootObject->getQuery())->getResults()->data->projects->nodes; // @phpstan-ignore-line
-        if (count($projects) == 0)
+        if(count($projects) == 0)
             return;
 
-        collect($projects[0]->repository->tree->blobs->nodes)->each(function ($repoFile) use ($selectFile, $dir)
-        {
-            if ($selectFile != null && $repoFile->name != $selectFile)
+        collect($projects[0]->repository->tree->blobs->nodes)->each(function($repoFile) use ($selectFile, $dir) {
+            if($selectFile != null && $repoFile->name != $selectFile)
                 return;
-            $fileName         = "/" . trim("$dir/$repoFile->name", " /");
-            $file             = $this->protectedFiles()->firstOrNew([
+            $fileName = "/" . trim("$dir/$repoFile->name", " /");
+            $file = $this->protectedFiles()->firstOrNew([
                 'path' => $fileName
             ]);
-            $shaValues        = is_array($file->sha_values) ? $file->sha_values : [];
-            $shaValues[]      = $repoFile->sha;
+            $shaValues = is_array($file->sha_values) ? $file->sha_values : [];
+            $shaValues[] = $repoFile->sha;
             $file->sha_values = array_unique($shaValues);
             $file->save();
         });
     }
 
-    public function participants() : \Illuminate\Support\Collection
+    public function participants(): \Illuminate\Support\Collection
     {
-        return $this->projects->reject(function (Project $project)
-        {
+        return $this->projects->reject(function(Project $project) {
             return $project->ownable_type == null;
-        })->map(function (Project $project)
-        {
-            return $project->owners()->map(fn (User $user) => [
+        })->map(function(Project $project) {
+            return $project->owners()->map(fn(User $user) => [
                 'project_status' => $project->status
             ]);
         })->flatten();
@@ -217,9 +214,9 @@ class Task extends Model
 
     public function grade(User $user = null)
     {
-        if ($user == null)
+        if($user == null)
             $user = auth()->user();
-        if ($this->grades()->where('user_id', $user->id)->first() != null)
+        if($this->grades()->where('user_id', $user->id)->first() != null)
             return $this->grades()->where('user_id', $user->id)->first();
     }
 
@@ -241,10 +238,78 @@ class Task extends Model
             ->selectName()
             ->selectRawBlob();
         $client = new Client(config('scalable.gitlab_url') . '/api/graphql', ["Authorization" => 'Bearer ' . config('scalable.gitlab_token')]);
-        $files  = $client->runQuery($rootObject->getQuery())->getResults()->data->projects->nodes[0]->repository->blobs->nodes; // @phpstan-ignore-line
-        if (count($files) == 0)
+        $files = $client->runQuery($rootObject->getQuery())->getResults()->data->projects->nodes[0]->repository->blobs->nodes; // @phpstan-ignore-line
+        if(count($files) == 0)
             return null;
 
         return $files[0]->rawBlob;
+    }
+
+    public function canStart(User $user, &$message = null): bool
+    {
+        if(!now()->isBetween($this->starts_at, $this->ends_at)) {
+            $message = 'The task cannot be started outside of the task time frame';
+            return false;
+        }
+
+        $groups = $user->groups()->where('course_id', $this->course_id)->get();
+        $usersInGroups = $groups->pluck('users')
+            ->flatten()
+            ->unique('id');
+
+        if(static::usersHaveBegunTasks($user->id, $this->id)->count() > 0)
+            return false;
+
+        if(static::usersHaveBegunTasks($usersInGroups->pluck('id'), $this->id)->count() > 0)
+            return false;
+
+        if(static::groupsHaveBegunTasks($groups->pluck('id'), $this->id)->count() > 0)
+            return false;
+
+        if($this->track_id == null)
+            return true;
+
+        return !$this->otherTrackHasBeenPicked(
+            collect($usersInGroups->pluck('id'))->add($user->id)->unique(),
+            $groups->pluck('id')
+        );
+    }
+
+    private function otherTrackHasBeenPicked($users, $groups): bool
+    {
+        $siblings = $this->track->siblings()->pluck('id');
+        $siblingTasks = Task::whereIn('track_id', $siblings)->get();
+
+        if ($siblingTasks->count() == 0)
+            return false;
+
+        $startedUserTasks = static::usersHaveBegunTasks($users, $siblingTasks->pluck('id'));
+        $startedGroupTasks = static::groupsHaveBegunTasks($groups, $siblingTasks->pluck('id'));
+
+        return $startedUserTasks->count() > 0 || $startedGroupTasks->count() > 0;
+    }
+
+    private static function usersHaveBegunTasks(array|int|Arrayable $userIds, array|int|Arrayable $taskIds): Collection
+    {
+        $userIds = Collection::wrap($userIds);
+        $taskIds = Collection::wrap($taskIds);
+
+        return Project::whereIn('task_id', $taskIds)->whereHasMorph(
+            'ownable',
+            User::class,
+            fn(Builder $query) => $query->whereIn('id', $userIds)
+        )->get();
+    }
+
+    private static function groupsHaveBegunTasks(array|int|Arrayable $groupIds, array|int|Arrayable $taskIds): Collection
+    {
+        $groupIds = Collection::wrap($groupIds);
+        $taskIds = Collection::wrap($taskIds);
+
+        return Project::whereIn('task_id', $taskIds)->whereHasMorph(
+            'ownable',
+            Group::class,
+            fn(Builder $query) => $query->whereIn('id', $groupIds)
+        )->get();
     }
 }
--- tests/Unit/Course/TaskTest.php
+++ tests/Unit/Course/TaskTest.php
@@ -1,12 +1,139 @@
 <?php
 
+use App\Models\Course;
+use App\Models\CourseTrack;
+use App\Models\Group;
+use App\Models\Project;
+use App\Models\Task;
+use App\Models\User;
+use Carbon\Carbon;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
-test('canStart returns false if now is before the start time');
-test('canStart returns false if now is after the end time');
-test('canStart returns true if now is after the start time and before the end time');
-test('canStart returns false if a group member has already begun the task');
-test('canStart returns false if the task has already been started');
-test('canStart returns false if the user is not on the same track');
-test('canStart returns true if the user is on the correct track');
-test('canStart returns false if all group members are not on the same track');
+uses(RefreshDatabase::class);
+
+beforeEach(function() {
+    $this->course = Course::factory()->create();
+    $this->task = Task::factory([
+        'starts_at' => Carbon::create(2022, 8, 8, 12),
+        'ends_at' => Carbon::create(2022, 8, 24, 23, 59, 59)
+    ])->for($this->course)->create();
+    $this->user = User::factory()->hasAttached($this->course)->create();
+    Carbon::setTestNow(Carbon::create(2022, 8, 16));
+});
+
+test('canStart returns false if now is before the start time', function() {
+    Carbon::setTestNow(Carbon::create(2022, 8, 3));
+    $message = null;
+    expect($this->task->canStart($this->user, $message))->toBeFalse();
+    expect($message)->toBe('The task cannot be started outside of the task time frame');
+});
+
+test('canStart returns false if now is after the end time', function() {
+    Carbon::setTestNow(Carbon::create(2022, 8, 29));
+
+    expect($this->task->canStart($this->user))->toBeFalse();
+});
+
+test('canStart returns true if now is after the start time and before the end time', function() {
+    expect($this->task->canStart($this->user))->toBeTrue();
+});
+
+test('canStart returns false if a group member has already begun the task', function () {
+    $user2 = User::factory()->hasAttached($this->course)->create();
+    Group::factory()->for($this->course)
+        ->hasAttached($this->user)
+        ->hasAttached($user2)
+        ->create();
+    Project::factory()->for($user2, 'ownable')->for($this->task)->createQuietly();
+
+    expect($this->task->canStart($this->user))->toBeFalse();
+});
+
+test('canStart returns false if a group has already begun the task', function () {
+    $group = Group::factory()->for($this->course)
+        ->hasAttached($this->user)
+        ->create();
+    Project::factory()->for($group, 'ownable')->for($this->task)->createQuietly();
+
+    expect($this->task->canStart($this->user))->toBeFalse();
+});
+
+test('canStart returns false if the task has already been started', function() {
+    Project::factory()->for($this->user, 'ownable')->for($this->task)->createQuietly();
+
+    expect($this->task->canStart($this->user))->toBeFalse();
+});
+
+test('canStart returns true if the task has not been started', function() {
+    expect($this->task->canStart($this->user))->toBeTrue();
+});
+
+test('canStart returns true if the group has not started the task', function() {
+    Group::factory()->for($this->course)
+        ->hasAttached($this->user)
+        ->create();
+
+    expect($this->task->canStart($this->user))->toBeTrue();
+});
+
+test('canStart returns false if the user is not on the same track', function () {
+    $root = CourseTrack::factory()->for($this->course)->create();
+    $track1 = CourseTrack::factory()->for($root, 'parent')->create();
+    $track2 = CourseTrack::factory()->for($root, 'parent')->create();
+
+    $task1ForTrack1 = Task::factory([
+        'starts_at' => Carbon::create(2022, 8, 8, 12),
+        'ends_at' => Carbon::create(2022, 8, 24, 23, 59, 59)
+    ])->for($this->course)->for($track1, 'track')->create();
+
+    $task2ForTrack2 = Task::factory([
+        'starts_at' => Carbon::create(2022, 8, 8, 12),
+        'ends_at' => Carbon::create(2022, 8, 24, 23, 59, 59)
+    ])->for($this->course)->for($track2, 'track')->create();
+
+    Project::factory()->for($this->user, 'ownable')->for($task2ForTrack2)->createQuietly();
+
+    expect($task1ForTrack1->canStart($this->user))->toBeFalse();
+});
+
+test('canStart returns true if the user is on the correct track', function() {
+    $root = CourseTrack::factory()->for($this->course)->create();
+    $track1 = CourseTrack::factory()->for($root, 'parent')->create();
+
+    $task1ForTrack1 = Task::factory([
+        'starts_at' => Carbon::create(2022, 8, 8, 12),
+        'ends_at' => Carbon::create(2022, 8, 24, 23, 59, 59)
+    ])->for($this->course)->for($track1, 'track')->create();
+
+    $task2ForTrack1 = Task::factory([
+        'starts_at' => Carbon::create(2022, 8, 8, 12),
+        'ends_at' => Carbon::create(2022, 8, 24, 23, 59, 59)
+    ])->for($this->course)->for($track1, 'track')->create();
+    Project::factory()->for($this->user, 'ownable')->for($task2ForTrack1)->createQuietly();
+
+    expect($task1ForTrack1->canStart($this->user))->toBeTrue();
+});
+
+test('canStart returns false if all group members are not on the same track', function() {
+    $root = CourseTrack::factory()->for($this->course)->create();
+    $track1 = CourseTrack::factory()->for($root, 'parent')->create();
+    $track2 = CourseTrack::factory()->for($root, 'parent')->create();
+    $group = Group::factory()->for($this->course)
+        ->hasAttached($this->user)
+        ->create();
+
+    $task1ForTrack1 = Task::factory([
+        'starts_at' => Carbon::create(2022, 8, 8, 12),
+        'ends_at' => Carbon::create(2022, 8, 24, 23, 59, 59)
+    ])->for($this->course)->for($track1, 'track')->create();
+
+    $task2ForTrack2 = Task::factory([
+        'starts_at' => Carbon::create(2022, 8, 8, 12),
+        'ends_at' => Carbon::create(2022, 8, 24, 23, 59, 59)
+    ])->for($this->course)->for($track2, 'track')->create();
+
+    Project::factory()->for($group, 'ownable')->for($task2ForTrack2)->createQuietly();
+
+    expect($task1ForTrack1->canStart($this->user))->toBeFalse();
+});
 
