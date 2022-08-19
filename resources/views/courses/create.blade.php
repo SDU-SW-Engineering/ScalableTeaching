af51ef1037949260fc1bca0f5bb4e7cf162a60ac
Christen Faurskov Andersen <cfa@enabled-robotics.com>
2022-08-19 10:46:00 +0200

    add feature test for course

--- resources/views/courses/create.blade.php
+++ resources/views/courses/create.blade.php
@@ -1,12 +1,18 @@
 @extends('master')
 
 @section('content')
-    <div class="px-6 pt-4 lg:max-w-xl container mx-auto">
+    <div class="px-6 pt-6 container mx-auto">
         <form action="{{ route('courses.store') }}" method="post">
             @csrf
-            <label>Name of course</label>
-            <input type="text" name="course-name">
-            <button type="submit">Create</button>
+            <div class="py-2 px-6 pt-2 lg:max-w-xl container mx-auto flex flex-col shadow-s bg-gray-200 dark:bg-gray-700 rounded-md">
+                <label class="px-1 text-sm text-lime-green-700 dark:text-gray-400">
+                    Create course
+                </label>
+                <input type="text" name="course-name" placeholder="course name" class="mb-3 bg-gray-50 flex-grow border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:outline-none focus:ring-2 focus:ring-lime-green-400  block w-full p-2.5 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-200">
+                <button class="flex-shrink-0 bg-transparent hover:bg-lime-green-500 text-lime-green-700 font-semi-bold hover:text-white py-2 px-4 border border-lime-green-500 hover:border-transparent rounded-lg" type="submit">
+                    Create
+                </button>
+            </div>
         </form>
     </div>
 @endsection
---
+++ tests/Feature/Course/CourseTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tests\Feature\Course;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+use function Pest\Laravel\actingAs;
+use function Tests\Feature\factory;
+
+uses(RefreshDatabase::class);
+
+it('allows admins to create courses', function () {
+    $admin = User::factory()->admin()->create();
+    actingAs($admin);
+    //$this->get('/courses')->assertDontSee('WebTechnologies');
+    $this->followingRedirects()->post('/courses', [
+        'course-name' => 'WebTechnologies'
+    ])->assertStatus(200)->assertSee('WebTechnologies');
+});
