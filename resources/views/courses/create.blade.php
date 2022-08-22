5b993a62e27820fdaed97cf17149a9471b3c21f9
Christen Faurskov Andersen <Chan719@student.sdu.dk>
2022-08-22 12:30:45 +0200

    make button white

--- resources/views/courses/create.blade.php
+++ resources/views/courses/create.blade.php
@@ -50,7 +50,7 @@
                 <input type="text" name="course-name" placeholder="course name"
                        class="mb-3 bg-gray-50 flex-grow border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:outline-none focus:ring-2 focus:ring-lime-green-400  block w-full p-2.5 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-200">
                 <button
-                    class="flex-shrink-0 bg-transparent hover:bg-lime-green-500 text-lime-green-700 font-semi-bold hover:text-white py-2 px-4 border border-lime-green-500 hover:border-transparent rounded-lg"
+                    class="flex-shrink-0 bg-white hover:bg-lime-green-500 text-lime-green-700 font-semi-bold hover:text-white py-2 px-4 border border-lime-green-500 hover:border-transparent rounded-lg"
                     type="submit">
                     Create
                 </button>
