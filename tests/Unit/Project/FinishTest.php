44f7ee7716fcc95f5b97d89b868802e3d028fe61
Niels Faurskov <niels@faurskov.dev>
2022-09-24 02:27:33 +0200

    Testing

--- tests/Unit/Project/FinishTest.php
+++ tests/Unit/Project/FinishTest.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Models\Course;
 use App\Models\Project;
+use App\Models\Task;
 use App\ProjectStatus;
 use Carbon\Carbon;
 use function Pest\Laravel\assertDatabaseHas;
@@ -10,7 +12,7 @@ uses(\Illuminate\Foundation\Testing\RefreshDatabase::class);
 it('sets the finished_at timestamp when a project is marked as finished', function() {
     Carbon::setTestNow(Carbon::create(2022, 1, 23, 12, 0, 0));
     /** @var Project $project */
-    $project = Project::factory()->createQuietly();
+    $project = Project::factory()->for(Task::factory()->for(Course::factory()))->createQuietly();
     $project->setProjectStatus(ProjectStatus::Finished);
 
     assertDatabaseHas('projects', [
