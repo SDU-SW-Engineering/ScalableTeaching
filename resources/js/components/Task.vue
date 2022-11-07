d6dd9d869e41fed9ccfddea1da04fc5c2d1c4b81
Christen97 <49652336+Christen97@users.noreply.github.com>
2022-11-07 12:30:45 +0100

    change exercise view

--- resources/js/components/Task.vue
+++ resources/js/components/Task.vue
@@ -61,6 +61,28 @@
                             Code
                         </span>
                     </a>
+                    <a class="text-gray-700 hover:bg-gray-100 hover:text-gray-800 dark:text-gray-400 dark:hover:bg-gray-500 dark:hover:text-gray-300 py-2 px-3 rounded-md font-semibold flex" target="_blank" :href="editRoute" v-if="editRoute !== ''">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 mr-1" fill="none" viewBox="0 0 24 24"
+                             stroke="currentColor" stroke-width="2">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                  d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"/>
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"/>
+                        </svg>
+                        <span>
+                            Edit Exercise
+                        </span>
+                    </a>
+                    <a class="text-gray-700 hover:bg-gray-100 hover:text-gray-800 dark:text-gray-400 dark:hover:bg-gray-500 dark:hover:text-gray-300 py-2 px-3 rounded-md font-semibold flex" target="_blank" :href="analytics" v-if="analytics !== ''">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 mr-1" fill="none"
+                             viewBox="0 0 24 24"
+                             stroke="currentColor" stroke-width="2">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                  d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"/>
+                        </svg>
+                        <span>
+                            Analytics
+                        </span>
+                    </a>
                     <button v-if="project != null" @click="tab = 'settings'"
                             :class="[tab === 'settings' ? 'bg-lime-green-100 dark:bg-gray-400 text-lime-green-700 dark:text-gray-100 dark:hover:text-gray-100 hover:text-lime-green-700' : 'text-gray-700 hover:bg-gray-100 hover:text-gray-800 dark:text-gray-400 dark:hover:bg-gray-500 dark:hover:text-gray-300']"
                             class="py-2 px-3 rounded-md font-semibold flex">
@@ -216,7 +238,7 @@ export default {
         SubTasks,
         Warning, BarChart, Overdue, Started, NotStarted, Settings, BuildTable, LineChart, Completed, Alert, Waiting
     },
-    props: ['task', 'grade', 'survey', 'pushes', 'project', 'progress', 'totalMyBuilds', 'totalBuilds', 'newProjectUrl', 'csrf', 'buildGraph', 'groups', 'userName', 'warning', 'subTasks', 'codeRoute'],
+    props: ['analytics', 'editRoute', 'task', 'grade', 'survey', 'pushes', 'project', 'progress', 'totalMyBuilds', 'totalBuilds', 'newProjectUrl', 'csrf', 'buildGraph', 'groups', 'userName', 'warning', 'subTasks', 'codeRoute'],
     methods: {
         startAssignment: async function (startAs) {
             let createAs = startAs == null ? this.startAs : startAs;
--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -2,9 +2,21 @@
 
 @section('content')
     <task :task="{{ $task }}"
+          @if(auth()->user()->is_admin ==  1)
+              edit-route="{{  route('courses.tasks.admin.preferences', [$course, $task]) }}"
+              analytics="{{  route('courses.tasks.admin.index', [$course, $task]) }}"
+          @elseif(auth()->user()->is_admin == 0)
+              edit-route="{{  null }}"
+              analytics="{{  null }}"
+          @endif
           code-route="{{ $codeRoute }}"
           :grade="{{ $task->grade(auth()->user()) ?? 'null' }}"
           :survey="{{ json_encode($survey) }}"
-          :sub-tasks="{{ json_encode($subTasks) }}" :project="{{ is_null($project) ? 'null' : $project}}" :progress="{{ json_encode($progress) }}"
-          :build-graph="{{ collect($buildGraph) }}" :pushes="{{ json_encode($pushes) }}" warning="{{ session()->has('warning') ? session('warning') : '' }}" :groups="{{ $availableGroups }}" user-name="{{ auth()->user()->name }}" :total-builds="{{ $builds->sum() }}" :total-my-builds="{{ $myBuilds->sum() }}" csrf="{{ csrf_token() }}" new-project-url="{{ $newProjectRoute }}"></task>
+          :sub-tasks="{{ json_encode($subTasks) }}" :project="{{ is_null($project) ? 'null' : $project}}"
+          :progress="{{ json_encode($progress) }}"
+          :build-graph="{{ collect($buildGraph) }}" :pushes="{{ json_encode($pushes) }}"
+          warning="{{ session()->has('warning') ? session('warning') : '' }}" :groups="{{ $availableGroups }}"
+          user-name="{{ auth()->user()->name }}" :total-builds="{{ $builds->sum() }}"
+          :total-my-builds="{{ $myBuilds->sum() }}" csrf="{{ csrf_token() }}"
+          new-project-url="{{ $newProjectRoute }}"></task>
 @endsection
