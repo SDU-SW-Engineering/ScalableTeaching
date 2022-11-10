40718a7221c8ff8262f161205afcb87503df34c7
Christen97 <49652336+Christen97@users.noreply.github.com>
2022-11-10 11:35:23 +0100

    extract index methods

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -54,29 +54,33 @@ class DashboardController extends Controller
         ]);
     }
 
+    public function authIndex() :view
+    {
+        $awaitingFeedback = auth()->user()->feedback()->with(['taskDelegation', 'project.task.course'])->unreviewed()->get()
+            ->filter(fn(ProjectFeedback $feedback) => $feedback->taskDelegation->deadline_at->isFuture());
+        $courses = auth()->user()->courses()->get();
+        $tasks = Task::whereIn('course_id', $courses->pluck('id'))->where('ends_at', '>=', now())->assignments()->orderBy('ends_at', 'asc')->visible()->get();
+        $nextAssignment = $tasks->first();
+        $courseAssignments = Task::assignments()->whereIn('course_id', $courses->pluck('id'))->get();
+        $exercises = Task::exercises()->whereIn('course_id', $courses->pluck('id'))->orderBy('starts_at', 'asc')->take(5)->visible()->get();
+
+        return view('dashboard', [
+            'courses'           => $courses,
+            'tasks'             => $tasks,
+            'exercises'         => $exercises,
+            'courseAssignments' => $courseAssignments,
+            'nextAssignment'    => $nextAssignment,
+            'bg'                => 'bg-gray-100 dark:bg-gray-700',
+            'awaitingFeedback'  => $awaitingFeedback,
+        ]);
+    }
     public function index(): view
     {
         if (Auth::check())
         {
-            $awaitingFeedback = auth()->user()->feedback()->with(['taskDelegation', 'project.task.course'])->unreviewed()->get()
-                ->filter(fn(ProjectFeedback $feedback) => $feedback->taskDelegation->deadline_at->isFuture());
-            $courses = auth()->user()->courses()->get();
-            $tasks = Task::whereIn('course_id', $courses->pluck('id'))->where('ends_at', '>=', now())->assignments()->orderBy('ends_at', 'asc')->visible()->get();
-            $nextAssignment = $tasks->first();
-            $courseAssignments = Task::assignments()->whereIn('course_id', $courses->pluck('id'))->get();
-            $exercises = Task::exercises()->whereIn('course_id', $courses->pluck('id'))->orderBy('starts_at', 'asc')->take(5)->visible()->get();
-
-            return view('dashboard', [
-                'courses'           => $courses,
-                'tasks'             => $tasks,
-                'exercises'         => $exercises,
-                'courseAssignments' => $courseAssignments,
-                'nextAssignment'    => $nextAssignment,
-                'bg'                => 'bg-gray-100 dark:bg-gray-700',
-                'awaitingFeedback'  => $awaitingFeedback,
-            ]);
+            return $this->authIndex();
         }
-        
+
         return $this->nonAuthIndex();
     }
 }
