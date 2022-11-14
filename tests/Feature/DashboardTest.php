7ad9b5467d418729e205718f8f06f8496082a4c7
Christen97 <49652336+Christen97@users.noreply.github.com>
2022-11-14 16:33:31 +0100

    Feature test for order of courses

--- tests/Feature/DashboardTest.php
+++ tests/Feature/DashboardTest.php
@@ -10,20 +10,37 @@ uses(RefreshDatabase::class);
 
 
 beforeEach(function() {
-    $this->course = Course::factory()->create();
-    $this->oldExercise = Task::factory()->exercise()->for($this->course)->create([
+    $this->oldCourse = Course::factory()->create([
+        'name'       => 'old course',
+        'created_at' => date('2022-11-10'),
+    ]);
+
+    $this->newCourse = Course::factory()->create([
+        'name'       => 'new course',
+        'created_at' => date('2022-11-13'),
+    ]);
+
+    $this->oldExercise = Task::factory()->exercise()->for($this->oldCourse)->create([
         'name'       => 'old exercise',
         'created_at' => date('2022-11-10'),
     ]);
-    $this->newExercise = Task::factory()->exercise()->for($this->course)->create([
+
+    $this->newExercise = Task::factory()->exercise()->for($this->oldCourse)->create([
         'name'       => 'new exercise',
         'created_at' => date('2022-11-13'),
     ]);
 });
 
-it('tests exercises are ordered by desc', function () {
-    $student = User::factory()->hasAttached($this->course)->create();
+it('tests courses are ordered by desc', function () {
+    $student = User::factory()->hasAttached([$this->oldCourse, $this->newCourse])->create();
     actingAs($student);
 
-    $this->assertEquals('new exercise', Task::exercises()->whereIn('course_id', $this->course->pluck('id'))->orderBy('created_at', 'desc')->visible()->first()->name);
+    $this->assertEquals('new course', $student->courses()->orderBy('created_at', 'desc')->first()->name);
+});
+
+it('tests exercises are ordered by desc', function () {
+    $student = User::factory()->hasAttached($this->oldCourse)->create();
+    actingAs($student);
+
+    $this->assertEquals('new exercise', Task::exercises()->whereIn('course_id', $this->oldCourse->pluck('id'))->orderBy('created_at', 'desc')->visible()->first()->name);
 });
