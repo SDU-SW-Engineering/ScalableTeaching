a5bc45f5914cf04b5516059b3749f470f96c583f
Magnus Jensen <magnushjensen.mail@gmail.com>
2024-01-17 19:42:17 +0100

    conflict mark as done and link repository

--- app/Modules/LinkRepository/LinkRepository.php
+++ app/Modules/LinkRepository/LinkRepository.php
@@ -2,6 +2,7 @@
 
 namespace App\Modules\LinkRepository;
 
+use App\Modules\MarkAsDone\MarkAsDone;
 use App\Modules\Module;
 use App\Modules\Settings;
 
@@ -14,6 +15,9 @@ class LinkRepository extends Module
                                       d="M13.19 8.688a4.5 4.5 0 011.242 7.244l-4.5 4.5a4.5 4.5 0 01-6.364-6.364l1.757-1.757m13.35-.622l1.757-1.757a4.5 4.5 0 00-6.364-6.364l-4.5 4.5a4.5 4.5 0 001.242 7.244"/>
                             </svg>';
     protected string $description = 'Links this repository to a GitLab repo.';
+    protected array $conflicts = [
+        MarkAsDone::class,
+    ];
 
     protected function loadSettings(): ?Settings
     {
