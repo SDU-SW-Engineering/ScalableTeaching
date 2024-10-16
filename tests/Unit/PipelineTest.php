d94bafc228cb8373db2d2ddaf9d850cd78a0d4f7
Magnus Jensen <magnushjensen.mail@gmail.com>
2024-10-16 15:26:52 +0200

    check subtask for null before creating it as point giving (#161)

--- tests/Unit/PipelineTest.php
+++ tests/Unit/PipelineTest.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Models\Course;
 use App\Models\Enums\PipelineStatusEnum;
 use App\Models\Pipeline;
+use App\Models\Project;
+use App\Models\Task;
 
 it('ensures a failing pipeline is not upgradable', function() {
     $pipeline = new Pipeline();
@@ -37,5 +40,47 @@ it('ensures a running pipeline is upgradable to success and failed', function() 
     expect($pipeline->isUpgradable(PipelineStatusEnum::Success))->toBeTrue();
     expect($pipeline->isUpgradable(PipelineStatusEnum::Failed))->toBeTrue();
     expect($pipeline->isUpgradable(PipelineStatusEnum::Pending))->toBeFalse();
+});
+
+it('skips jobs that are successful but not tracked', function() {
+    \Illuminate\Support\Facades\Log::spy();
+
+    /** @var Project $project */
+    $project = Project::factory()->for(Task::factory()->for(Course::factory()))->create();
+
+    $pipeline = new Pipeline();
+    $pipeline->status = PipelineStatusEnum::Running;
+    $pipeline->project = $project;
+
+    \Illuminate\Support\Facades\Log::shouldReceive('debug')->once();
+    $pipeline->process(
+        startedAt: now(),
+        status: PipelineStatusEnum::Success,
+        duration: 10,
+        queueDuration: 5,
+        succeedingBuilds: ['test 11 equals [10, 1]']
+    );
+});
+
+it('does not skip jobs that are successful and tracked', function() {
+    \Illuminate\Support\Facades\Log::spy();
+
+
+    $subTasks = createSubTasks([
+        ['title' => 'test 11 Equals [10, 1]', 'points' => 50],
+    ]);
+    /** @var Project $project */
+    $project = Project::factory()->for(Task::factory(['sub_tasks' => $subTasks])->for(Course::factory()))->create();
+
+    $pipeline = Pipeline::factory()->running()->for($project)->create();
+
+    \Illuminate\Support\Facades\Log::shouldReceive('debug')->never();
+    $pipeline->process(
+        startedAt: now(),
+        status: PipelineStatusEnum::Success,
+        duration: 10,
+        queueDuration: 5,
+        succeedingBuilds: ['test 11 equals [10, 1]']
+    );
 });
 
