1f6d0054b63935be9f166fbf3a465914e93149bc
Mads W. Pedersen <mads456dk@gmail.com>
2025-02-20 17:27:02 +0100

    Fixed bug where Scalable atempted to add already added user to a repo

--- tests/Unit/App/Models/TaskTest.php
+++ tests/Unit/App/Models/TaskTest.php
@@ -39,7 +39,7 @@ it('returns the correct ID of the gitlab project', function () {
     expect($this->task->getGitlabProjectId())->toBe(9033);
 });
 
-it('adds user to template repository if task is a "code task"', function () {
+it('adds single user to template repository if task is a "code task"', function () {
     addLinkToRepositoryModule($this);
     expect($this->task->isCodeTask())->toBeTrue();
 
@@ -57,7 +57,51 @@ it('adds user to template repository if task is a "code task"', function () {
     $mockProjects->shouldReceive('addMember')
         ->with('9033', $user->gitlab_id, 20)
         ->once()
-        ->andReturnNull();  // Prevent actual execution
+        ->andReturnNull();
+
+
+    $mockProjects->shouldReceive('allMembers')
+        ->with('9033')
+        ->once()
+        ->andReturn([[]]);  // Prevent actual execution
+
+    // Make Laravel use the mocked instance
+    app()->instance(GitLabManager::class, $mockGitLabManager);
+
+    // Act
+    $this->task->createProject($user);
+    assertDatabaseHas('projects', [
+        'task_id'              => $this->task->id,
+        'ownable_type'         => 'App\Models\User',
+        'ownable_id'           => $user->id,
+    ]);
+});
+
+it('does not add user to template repository if the user is already a member', function () {
+    addLinkToRepositoryModule($this);
+    expect($this->task->isCodeTask())->toBeTrue();
+
+    $user = User::factory()->create();
+
+    // Mock GitLabManager & Projects
+    $mockGitLabManager = Mockery::mock(GitLabManager::class);
+    $mockProjects = Mockery::mock(Projects::class);
+
+    // Ensure that `projects()` method returns the mock
+    $mockGitLabManager->shouldReceive('projects')
+        ->andReturn($mockProjects);
+
+    $mockProjects->shouldReceive('allMembers')
+        ->with('9033')
+        ->once()
+        ->andReturn([['id' => $user->gitlab_id]]);
+
+    // Mock `addMember` before execution
+    $mockProjects->shouldReceive('addMember')
+        ->with('9033', $user->gitlab_id, 20)
+        ->never()
+        ->andReturnNull();
+
 
     // Make Laravel use the mocked instance
     app()->instance(GitLabManager::class, $mockGitLabManager);
@@ -97,6 +141,58 @@ it('adds all users of a group to template repository if task is a "code task"', 
         ->with('9033', Mockery::any(), 20)
         ->times(4)
         ->andReturnNull();  // Prevent actual execution
+
+    $mockProjects->shouldReceive('allMembers')
+        ->with('9033')
+        ->times(4)
+        ->andReturn([[]]);  // Prevent actual execution
+
+
+    // Make Laravel use the mocked instance
+    app()->instance(GitLabManager::class, $mockGitLabManager);
+
+    // Act
+    $this->task->createProject($group);
+
+    assertDatabaseHas('projects', [
+        'task_id'              => $this->task->id,
+        'ownable_type'         => 'App\Models\Group',
+        'ownable_id'           => $group->id,
+    ]);
+});
+
+it('only adds members of a group if they are not already a member of the template repository', function () {
+    addLinkToRepositoryModule($this);
+    expect($this->task->isCodeTask())->toBeTrue();
+    $group = Group::factory([
+        'name'      => 'Example Group',
+    ])->for($this->course)->create();
+
+    // Create four users
+    $users = User::factory()->count(4)->create();
+
+    // Attach users to the group
+    $group->members()->attach($users);
+
+    // Mock GitLabManager & Projects
+    $mockGitLabManager = Mockery::mock(GitLabManager::class);
+    $mockProjects = Mockery::mock(Projects::class);
+
+    // Ensure that `projects()` method returns the mock
+    $mockGitLabManager->shouldReceive('projects')
+        ->andReturn($mockProjects);
+
+    // Mock `addMember` before execution
+    $mockProjects->shouldReceive('addMember')
+        ->with('9033', Mockery::any(), 20)
+        ->times(3)
+        ->andReturnNull();  // Prevent actual execution
+
+    $mockProjects->shouldReceive('allMembers')
+        ->with('9033')
+        ->times(4)
+        ->andReturn([['id' => $group->members()->first()->gitlab_id]]);
+
 
     // Make Laravel use the mocked instance
     app()->instance(GitLabManager::class, $mockGitLabManager);
