8aaa19e1d731a28005d9b49261d5a9f43cea4599
Mads W. Pedersen <mads456dk@gmail.com>
2025-02-20 11:28:14 +0100

    Tasks in task management now link to the management page for the task

--- resources/js/components/Course/Management/Exercises.vue
+++ resources/js/components/Course/Management/Exercises.vue
@@ -158,6 +158,7 @@
                                     </svg>
                                 </button>
                             </div>
+                            <!-- Possibly deprecated code? (Line 162 - 223) -->
                             <div
                                 v-if="!reorganizing"
                                 :class="[
@@ -182,6 +183,7 @@
                                         class="text-sm"
                                         v-text="exercise.name"
                                     ></span>
+                                    TEKST IKKE HER
                                     <svg
                                         v-if="exercise.visible"
                                         xmlns="http://www.w3.org/2000/svg"
@@ -233,7 +235,7 @@
                                 group="inner"
                                 draggable=".item:not(.not-draggable-item)"
                             >
-                                <div
+                                <a
                                     :class="[
                                         group.name === null
                                             ? 'bg-gray-400 dark:bg-gray-600'
@@ -241,6 +243,7 @@
                                     ]"
                                     class="flex items-center justify-between px-3 py-1.5 rounded text-white dark:text-gray-100 mb-4 last:mb-0 cursor-pointer item"
                                     v-for="exercise in group.exercises"
+                                    :href="exercise.manage"
                                 >
                                     <span
                                         class="text-sm"
@@ -281,7 +284,7 @@
                                             d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88"
                                         />
                                     </svg>
-                                </div>
+                                </a>
                                 <div
                                     slot="header"
                                     class="btn-group list-group-item"
@@ -311,9 +314,10 @@
                     class="py-4 rounded-md bg-gray-300 px-3"
                     draggable=".item:not(.not-draggable-item)"
                 >
-                    <div
+                    <a
                         class="bg-gray-400 dark:bg-gray-700 flex items-center justify-between px-3 py-1.5 rounded text-white dark:text-gray-100 mb-4 last:mb-0 cursor-pointer item"
                         v-for="task in sidebarGroup"
+                        :href="task.manage"
                     >
                         <span class="text-sm" v-text="task.name"></span>
                         <svg
@@ -351,7 +355,7 @@
                                 d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88"
                             />
                         </svg>
-                    </div>
+                    </a>
                     <div
                         slot="header"
                         class="btn-group list-group-item"
