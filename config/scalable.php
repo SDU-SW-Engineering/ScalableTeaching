791ab23afe322288b0274f9d61aa3c320bf9eb9d
Mads W. Pedersen <mads456dk@gmail.com>
2025-03-24 14:53:33 +0100

    Fixed config/scalable.php

--- config/scalable.php
+++ config/scalable.php
@@ -1,9 +1,9 @@
 <?php
 
 return [
-    'webhook_secret' => env('SCALABLE_SECRET', 'scalable'),
-    'gitlab_token'   => env('GITLAB_ACCESS_TOKEN'),
-    'gitlab_url'     => env('GITLAB_URL'),
+    'webhook_secret'   => env('SCALABLE_SECRET', 'scalable'),
+    'gitlab_token'     => env('GITLAB_ACCESS_TOKEN'),
+    'gitlab_url'       => env('GITLAB_URL'),
     'gitlab_group'     => env('GITLAB_GROUP'),
-    'webhook_url'     => env('GITLAB_WEBHOOK_URL'),
+    'webhook_url'      => env('GITLAB_WEBHOOK_URL'),
 ];
