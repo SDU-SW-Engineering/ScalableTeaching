32702adc801a83a07a82e566259e459d46141d2d
Mads W. Pedersen <mads456dk@gmail.com>
2025-03-28 09:48:54 +0100

    Sligthly changed the how Scalable waits for the Girlab to respond

--- app/Listeners/GitLab/Project/UnprotectDefaultBranch.php
+++ app/Listeners/GitLab/Project/UnprotectDefaultBranch.php
@@ -38,21 +38,23 @@ class UnprotectDefaultBranch implements ShouldQueue
      */
     public function handle(ProjectCreated $event): void
     {
-        if ( ! $event->project->task->isTemplateTask()) return;
+        if (!$event->project->task->isTemplateTask()) return;
 
         Log::info("Unprotecting default branch for project {$event->project->id}");
 
         $gitLabManager = app(GitLabManager::class);
 
         $attempts = 3;
-        do // The system is sometimes too fast for the Gitlab server,
-        {  // this buys the Gitlab server some more time before Scalable moves on.
-            usleep(100000);
-            $project = $gitLabManager->projects()->show($event->project->gitlab_project_id);
-            $attempts--;
-        } while (($project['import_error'] != null || $project['import_status'] != 'finished') && $attempts > 0);
 
-        if($project['import_error'] != null || $project['import_status'] != 'finished')
+        $project = $gitLabManager->projects()->show($event->project->gitlab_project_id);
+
+
+        while (($project['import_error'] != null || $project['import_status'] != 'finished') && $attempts > 0)
+        { // The system is sometimes too fast for the Gitlab server, this buys the Gitlab server some more time before Scalable moves on.
+            usleep(50000);
+            $attempts--;
+        }
+        if ($project['import_error'] != null || $project['import_status'] != 'finished')
             throw new Exception("Import not fully done yet.");
 
         $gitLabManager->repositories()->unprotectBranch($event->project->gitlab_project_id, $project['default_branch']);
