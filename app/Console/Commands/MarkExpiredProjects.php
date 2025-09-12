d68c84e6746f1626b37f811756c26aa44d913e99
Mads W. Pedersen <mads456dk@gmail.com>
2025-09-12 10:55:31 +0200

    PHPLinter changes

--- app/Console/Commands/MarkExpiredProjects.php
+++ app/Console/Commands/MarkExpiredProjects.php
@@ -48,20 +48,22 @@ class MarkExpiredProjects extends Command
             $this->info("Marking tasks under [{$task->course->name}] $task->name");
             $overDueTime = $task->ends_at->addMinutes(5);
 
-            if ( ! now()->isAfter($overDueTime)){
+            if ( ! now()->isAfter($overDueTime))
+            {
                 continue;
             }
 
-            if (!$task->module_configuration->isEnabled(MarkAsDone::class) && !$task->module_configuration->isEnabled(Template::class)){
+            if ( ! $task->module_configuration->isEnabled(MarkAsDone::class) && ! $task->module_configuration->isEnabled(Template::class))
+            {
                 $this->info("Skipping task $task->name ($task->id). This task is not completable and can thus not be finished nor overdue.");
                 continue;
             }
 
             $count = $task->projects()->where('status', 'active')->withTrashed()->each(function(Project $project) {
-                if ($project->pushes()->count() == 0){ // If a project has no pushes, and we are after the deadline then the project must not have been handed in thus we set it to overdue
-                    $project->setProjectStatus(ProjectStatus::Overdue);
-                } else { // If there are pushes, and we are after the deadline then the project must be finished
-                    $project->setProjectStatus(ProjectStatus::Finished);
+                if ($project->pushes()->count() == 0) // If a project has no pushes, and we are after the deadline then the project must not have been handed in thus we set it to overdue
+                {$project->setProjectStatus(ProjectStatus::Overdue);
+                } else // If there are pushes, and we are after the deadline then the project must be finished
+                {$project->setProjectStatus(ProjectStatus::Finished);
                 }
             });
             $this->info("Marked $count projects as overdue for task $task->name ($task->id).");
